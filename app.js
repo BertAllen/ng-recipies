46e00da39d95a336bc80d10b2d5330c4d0694998
BertAllen <mrlaughingdragon@aol.com>
2016-02-18 16:22:34 -0700

    git test

---
+++ app.js
@@ -0,0 +1,18 @@
+var app = angular.module("RecipesController", []);
+//app.controller("MainController", function(){
+app.controller("BigNest", function ($scope) {
+    $scope.name = "Peter";
+    $scope.user = { name: "Parker" };
+});
+
+app.controller("LittleNest", function ($scope) {
+});
+
+app.directive("myWidget", function () {
+    return {
+        restrict: "E",
+        transclude: true,
+        template: "<div ng-transclude><h3>Transcluded Heading</h3></div>"
+    };
+});
+//})
